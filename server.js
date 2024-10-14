0da82db1fb73562fc6d2037700bd76d683d78792
Sapinderjit Kaur <er.sapinder.bal93@gmail.com>
2024-10-14 15:23:57 -0400

    updated server.js

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+app.use(express.static('public'));
+
 const HTTP_PORT = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
